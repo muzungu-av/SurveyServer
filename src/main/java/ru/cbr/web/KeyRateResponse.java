db96581e851571c9e76bddcb9654ee259a1f44c0
muzungu.av <muzungu.av@gmail.com>
2023-03-18 14:28:01 +0300

    init parse xml and create classes

---
+++ src/main/java/ru/cbr/web/KeyRateResponse.java
@@ -0,0 +1,130 @@
+
+package ru.cbr.web;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="KeyRateResult" minOccurs="0">
+ *           &lt;complexType>
+ *             &lt;complexContent>
+ *               &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *                 &lt;sequence>
+ *                   &lt;any/>
+ *                 &lt;/sequence>
+ *               &lt;/restriction>
+ *             &lt;/complexContent>
+ *           &lt;/complexType>
+ *         &lt;/element>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "keyRateResult"
+})
+@XmlRootElement(name = "KeyRateResponse")
+public class KeyRateResponse {
+
+    @XmlElement(name = "KeyRateResult")
+    protected KeyRateResponse.KeyRateResult keyRateResult;
+
+    /**
+     * Gets the value of the keyRateResult property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link KeyRateResponse.KeyRateResult }
+     *     
+     */
+    public KeyRateResponse.KeyRateResult getKeyRateResult() {
+        return keyRateResult;
+    }
+
+    /**
+     * Sets the value of the keyRateResult property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link KeyRateResponse.KeyRateResult }
+     *     
+     */
+    public void setKeyRateResult(KeyRateResponse.KeyRateResult value) {
+        this.keyRateResult = value;
+    }
+
+
+    /**
+     * <p>Java class for anonymous complex type.
+     * 
+     * <p>The following schema fragment specifies the expected content contained within this class.
+     * 
+     * <pre>
+     * &lt;complexType>
+     *   &lt;complexContent>
+     *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+     *       &lt;sequence>
+     *         &lt;any/>
+     *       &lt;/sequence>
+     *     &lt;/restriction>
+     *   &lt;/complexContent>
+     * &lt;/complexType>
+     * </pre>
+     * 
+     * 
+     */
+    @XmlAccessorType(XmlAccessType.FIELD)
+    @XmlType(name = "", propOrder = {
+        "any"
+    })
+    public static class KeyRateResult {
+
+        @XmlAnyElement(lax = true)
+        protected Object any;
+
+        /**
+         * Gets the value of the any property.
+         * 
+         * @return
+         *     possible object is
+         *     {@link Object }
+         *     
+         */
+        public Object getAny() {
+            return any;
+        }
+
+        /**
+         * Sets the value of the any property.
+         * 
+         * @param value
+         *     allowed object is
+         *     {@link Object }
+         *     
+         */
+        public void setAny(Object value) {
+            this.any = value;
+        }
+
+    }
+
+}
---
+++ src/main/java/ru/cbr/web/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Веб сервис для получения ежедневных данных ver 21.02.2023
+ * прочти https://anteastra.blogspot.com/2012/12/java-web-sercice.html
+ */
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://web.cbr.ru/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package ru.cbr.web;
